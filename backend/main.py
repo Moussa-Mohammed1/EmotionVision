a2250ca1283972d8a969328c87c0c982dac2a58d
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-05-03 16:29:47 +0100

    feat(models): add haar cascade face detection and implement POST method for detecting face

--- backend/main.py
+++ backend/main.py
@@ -7,3 +7,17 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message: welcome to emotion recognition app"}
+
+
+@app.post("/detect-face")
+async def detect_face(file: UploadFile = File(...)):
+    content = await file.read()
+    nparr = np.frombuffer(content, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    return {
+        "faces_detected": len(faces)
+    }
