c77e961ef919cb018426c4ee8014eac3201ca6ed
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-05-04 22:06:30 +0100

    feat(homepage): enhance file upload functionality with loading state and display of prediction results

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,11 +2,20 @@ import { useState } from "react";
 import api from "../api/axios";
 
 
+type FacePrediction = {
+    bbox: [number, number, number, number];
+    emotion: string;
+    confidence: number;
+};
+
 function Home() {
     const [file, setFile] = useState<File | null>(null)
-
+    const [message, setMessage] = useState<string>("");
+    const [faces, setFaces] = useState<FacePrediction[]>([]);
+    const [loading, setLoading] = useState<Boolean>(false);
     const uploadFile = async () => {
         if (!file) return;
+        setLoading(true)
         const form = new FormData();
         form.append("file", file);
         try {
@@ -14,15 +23,37 @@ function Home() {
                 headers: { 'Content-Type': 'multipart/form-data' }
             });
             console.log(res?.data);
+            setMessage(res?.data.message);
+            setFaces(res?.data.faces ?? []);
         } catch (error) {
-            console.error('Upload failed:', error);
+            setMessage(`Upload failed: ${error}`);
+            setFaces([]);
+        }finally
+        {
+            setLoading(false)
         }
     }
 
     return (
         <div>
-            <input type="file" onChange={(e) => setFile(e.target.files?.[0] || null)} />
-            <button onClick={uploadFile} disabled={!file}>Send</button>
+            <div>
+                <input type="file" onChange={(e) => setFile(e.target.files?.[0] || null)} />
+                <button onClick={uploadFile} disabled={!file}>
+                    {loading ? 'Sending...' : 'Send'}
+                    
+                </button>
+            </div>
+            <p className="pr">
+                {message}
+            </p>
+            <div>
+                {faces.map((face, index) => (
+                    <p key={`${face.emotion}-${index}`}>
+                        Face {index + 1}: {face.emotion} ({Math.round(face.confidence * 100)}%)
+                    </p>
+                ))}
+            </div>
+
         </div>
     )
 }
